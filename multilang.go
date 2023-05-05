17512d8a5dadeb2fb2f1b15b6c4401f065ef8fe5
Kyriakos Milad <contact.kyriakos@gmail.com>
2023-05-06 01:02:27 +0200

    Set and get dictionary

--- multilang.go
+++ multilang.go
@@ -6,35 +6,43 @@ type (
 )
 
 var (
-	dict = map[string]*LanguageDictionary{}
+	dict = &Dictionary{}
 )
 
 func Set(lang string, key string, value string) {
-	if l, ok := dict[lang]; ok {
+	if l, ok := (*dict)[lang]; ok {
 		(*l)[key] = value
 		return
 	}
 
-	l := dict[lang]
+	l := (*dict)[lang]
 	l = &LanguageDictionary{}
 	(*l)[key] = value
 }
 
 func Get(lang string, key string) (string, bool) {
-	if _, ok := dict[lang]; !ok {
+	if _, ok := (*dict)[lang]; !ok {
 		return "", false
 	}
 
-	l := dict[lang]
+	l := (*dict)[lang]
 	val, ok := (*l)[key]
 	return val, ok
 }
 
 func SetLangDict(lang string, landDict *LanguageDictionary) {
-	dict[lang] = landDict
+	(*dict)[lang] = landDict
 }
 
 func GetLangDict(lang string, key string) (*LanguageDictionary, bool) {
-	l, ok := dict[lang]
+	l, ok := (*dict)[lang]
 	return l, ok
+}
+
+func SetDict(d *Dictionary) {
+	dict = d
+}
+
+func GetDict() *Dictionary {
+	return dict
 }
